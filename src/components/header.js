48c65f30aab0af645d3b7e90d46776c8a73fad6d
schak94 <schak94.sc@gmail.com>
2023-07-16 22:03:58 +0530

    episode-10

--- src/components/header.js
+++ src/components/header.js
@@ -6,18 +6,18 @@ import useOnlineStatus from "../utils/useOnlineStatus";
     const [loginButton, setLogin] = useState("Login");
     const onLine = useOnlineStatus();
     return(
-        <div className="flex justify-between p-1 m-1 shadow-lg">
+        <div className=" bg-green-200 flex justify-between py-1 mb-1 shadow-lg shadow-green-200/50">
             <div className="logo-container">
-                <img className="w-24" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
+                <img className=" w-16" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"/>
             </div>
             <div className="flex items-center">
                 <ul className="flex p-1 m-1">
-                    <li className="px-3 text-sm">{onLine ? "🟢":"🔴"}</li>
-                    <li className="px-3 text-sm"> <Link to="/">Home</Link></li>
-                    <li className="px-3 text-sm"> <Link to="/about">About</Link></li>
-                    <li className="px-3 text-sm"> <Link to="/contact">Contact Us</Link></li>
-                    <li className="px-3 text-sm"> <Link to="/">Cart</Link></li>
-                    <button className="text-sm" onClick={()=>{
+                    <li className="px-2 text-sm">{onLine ? "🟢":"🔴"}</li>
+                    <li className="px-2 text-sm"> <Link to="/">Home</Link></li>
+                    <li className="px-2 text-sm"> <Link to="/about">About</Link></li>
+                    <li className="px-2 text-sm"> <Link to="/contact">Contact Us</Link></li>
+                    <li className="px-2 text-sm"> <Link to="/">Cart</Link></li>
+                    <button className="text-sm px-4" onClick={()=>{
                         setLogin((pre)=>{
                             return pre==="Login"?"Logout":"Login";
                         })
